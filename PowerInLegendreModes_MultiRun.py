a49e148ee08430609e6ed42be6c1a3b25d7f4fd5
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-01-30 18:38:18 -0500

    Added Root dir

--- PowerInLegendreModes_MultiRun.py
+++ PowerInLegendreModes_MultiRun.py
@@ -18,14 +18,14 @@ if __name__ == '__main__':
     for ID in range( IDs.shape[0] ):
 
         tS, RsAve, RsMin, RsMax, P0, P1, P2, P3, P4 \
-          = np.loadtxt( '.' + IDs[ID] \
+          = np.loadtxt( Root + '.' + IDs[ID] \
                           + '_DivV2_0.80-0.90_PowersInLegendreModes.dat' )
 
         ind = np.where( tS < 3.0e2 )[0]
 
-        tS = np.copy( tS[ind] )
+        tS    = np.copy( tS   [ind] )
         RsAve = np.copy( RsAve[ind] )
-        P1 = np.copy( P1[ind] )
+        P1    = np.copy( P1   [ind] )
 
         axs[0].plot( tS, RsAve, label = IDs[ID][0:2] )
         axs[1].semilogy( tS, P1 )
