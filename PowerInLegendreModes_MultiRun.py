4dbb7699a1dc4540f17525ef3acb0d7ff8d86c3c
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-01-30 18:34:07 -0500

    Added PowerInLegendreModes_MultiRun.py

---
+++ PowerInLegendreModes_MultiRun.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use( './Publication.sty' )
+
+if __name__ == '__main__':
+
+    Root = './'
+
+    IDs = np.array( ['NR2D_M1.4_Mdot0.3_Rs180_PA1.00e-06_nX640x064', \
+                     'GR2D_M1.4_Mdot0.3_Rs180_PA1.00e-06_nX640x064' ], np.str )
+
+    fig, axs = plt.subplots( 2, 1 )
+
+    fig.suptitle( IDs[0][5:] )
+
+    for ID in range( IDs.shape[0] ):
+
+        tS, RsAve, RsMin, RsMax, P0, P1, P2, P3, P4 \
+          = np.loadtxt( '.' + IDs[ID] \
+                          + '_DivV2_0.80-0.90_PowersInLegendreModes.dat' )
+
+        ind = np.where( tS < 3.0e2 )[0]
+
+        tS = np.copy( tS[ind] )
+        RsAve = np.copy( RsAve[ind] )
+        P1 = np.copy( P1[ind] )
+
+        axs[0].plot( tS, RsAve, label = IDs[ID][0:2] )
+        axs[1].semilogy( tS, P1 )
+
+    axs[0].legend()
+    axs[0].xaxis.set_visible( False )
+    axs[1].set_xlabel( 'Time [ms]' )
+    axs[1].set_ylabel( r'$P_{1}$ [cgs]' )
+    axs[0].set_ylabel( r'$\left<R_{s}\right>$ [km]' )
+
+    plt.subplots_adjust( hspace = 0.0 )
+
+    plt.show()
+#    plt.savefig( 'fig.PowerInLegendreModes_MultiRun.png', dpi = 300 )
+    plt.close()
+
