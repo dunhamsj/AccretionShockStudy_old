28968a6590385eb91d5819f2d155145867b57d7b
Sam Dunham <9784969+samueljdunham@users.noreply.github.com>
2022-03-24 12:33:39 -0500

    modification: plot deviation of shock radius instead of shock radius

--- PowerInLegendreModes_MultiRun.py
+++ PowerInLegendreModes_MultiRun.py
@@ -8,26 +8,32 @@ if __name__ == '__main__':
 
     Root = './'
 
-    IDs = np.array( ['NR2D_M2.8_Mdot0.3_Rs180_PA1.00e-06', \
-                     'GR2D_M2.8_Mdot0.3_Rs180_PA1.00e-06' ], str )
-
     fig, axs = plt.subplots( 2, 1 )
 
-    fig.suptitle( IDs[0][5:] )
+    suptitle = 'GR_Mdot0.3_Rs150'
+    IDs = np.array( ['GR2D_M1.4_Mdot0.3_Rs150', \
+                     'GR2D_M2.0_Mdot0.3_Rs150', \
+                     'GR2D_M2.8_Mdot0.3_Rs150' ], str )
 
+    #suptitle = 'GR_M2.8_Mdot0.3'
+    #IDs = np.array( ['GR2D_M2.8_Mdot0.3_Rs120', \
+    #                 'GR2D_M2.8_Mdot0.3_Rs150', \
+    #                 'GR2D_M2.8_Mdot0.3_Rs180' ], str )
+
+    fig.suptitle( suptitle )
     for ID in range( IDs.shape[0] ):
 
         tS, RsAve, RsMin, RsMax, P0, P1, P2, P3, P4 \
           = np.loadtxt( Root + '.' + IDs[ID] \
                           + '_DivV2_0.80-0.90_PowersInLegendreModes.dat' )
 
-        ind = np.where( tS < 3.0e2 )[0]
+        ind = np.where( tS < 1.5e2 )[0]
 
         tS    = np.copy( tS   [ind] )
         RsAve = np.copy( RsAve[ind] )
         P1    = np.copy( P1   [ind] )
 
-        axs[0].plot( tS, RsAve, label = IDs[ID][0:2] )
+        axs[0].plot( tS, ( RsAve - RsAve[0] ) / RsAve[0], label = IDs[ID] )
         axs[1].semilogy( tS, P1 )
 
     axs[0].legend()
@@ -37,11 +43,12 @@ if __name__ == '__main__':
     axs[1].grid()
     axs[1].set_xlabel( 'Time [ms]' )
     axs[1].set_ylabel( r'$P_{1}$ [cgs]' )
-    axs[0].set_ylabel( r'$\left<R_{s}\right>$ [km]' )
+    axs[0].set_ylabel( r'$\left(\left<R_{s}\right>-\left<R_{s,0}\right>\right)/\left<R_{s,0}\right>$' )
 
     plt.subplots_adjust( hspace = 0.0 )
 
 #    plt.show()
-    plt.savefig( 'fig.PowerInLegendreModes_MultiRun.png', dpi = 300 )
+    plt.savefig( 'fig.PowerInLegendreModes_MultiRun_{:}.png'.format \
+                 ( suptitle ), dpi = 300 )
     plt.close()
 
