ab727fd66e914f5bdb114d9186b61b58aefbf17b
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-03-27 19:00:32 -0400

    backup /home/kkadoogan/Work/Analysis/AccretionShockStudy on Sun Mar 27 07:00:32 PM EDT 2022

--- FrequencyComparison.py
+++ FrequencyComparison.py
@@ -100,41 +100,45 @@ plt.close()
 #plt.savefig( 'fig.FrequencyComparison_1D_Mass.png', dpi = 300 )
 #plt.close()
 
-## Heatmap
-#
-#extent = [ 0, Rs.shape[0], 0, M.shape[0] ]
-#
-#fig, ax = plt.subplots( 1, 1 )
-#
-#im = ax.imshow( ( T_GR - T_GR ) / T_GR, \
-#                origin = 'lower', \
-#                extent = extent, \
-#                cmap = 'RdBu', \
+# Heatmap
+
+extent = [ 0, Rs.shape[0], 0, M.shape[0] ]
+
+fig, ax = plt.subplots( 1, 1 )
+
+Data = ( T_NR - T_GR ) / T_NR
+
+print( Data.max() )
+print( Data.min() )
+im = ax.imshow( Data, \
+                origin = 'lower', \
+                extent = extent, \
+                cmap = 'RdBu', \
 #                vmin = -0.01, vmax = +0.01, \
-#                interpolation = 'bilinear', \
-#                aspect = 'auto' )
-#yticklabels = M
-#xticklabels = Rs
-#xticks = np.empty( Rs.shape[0], np.float64 )
-#yticks = np.empty( M.shape [0], np.float64 )
-#for i in range( xticks.shape[0] ):
-#    j = i + 1
-#    xticks[i] = ( 2.0 * np.float64( j ) - 1.0 ) / 2.0
-#for i in range( yticks.shape[0] ):
-#    j = i + 1
-#    yticks[i] = ( 2.0 * np.float64( j ) - 1.0 ) / 2.0
-#ax.set_xticks( xticks )
-#ax.set_yticks( yticks )
-#ax.set_xticklabels( xticklabels )
-#ax.set_yticklabels( yticklabels )
-#
-#ax.set_xlabel( r'$R_{s}\ \left[\mathrm{km}\right]$' )
-#ax.set_ylabel( r'$M\ \left[\mathrm{M}_{\odot}\right]$' )
-#
-#cbar = fig.colorbar( im )
-#cbar.set_label( r'$( T_{\mathrm{GR}} - T_{\mathrm{NR}} ) / T_{\mathrm{GR}}$' )
-##plt.show()
-#plt.savefig( 'fig.FrequencyComparison_HeatMap.png', dpi = 300 )
+                interpolation = 'bilinear', \
+                aspect = 'auto' )
+yticklabels = M
+xticklabels = Rs
+xticks = np.empty( Rs.shape[0], np.float64 )
+yticks = np.empty( M.shape [0], np.float64 )
+for i in range( xticks.shape[0] ):
+    j = i + 1
+    xticks[i] = ( 2.0 * np.float64( j ) - 1.0 ) / 2.0
+for i in range( yticks.shape[0] ):
+    j = i + 1
+    yticks[i] = ( 2.0 * np.float64( j ) - 1.0 ) / 2.0
+ax.set_xticks( xticks )
+ax.set_yticks( yticks )
+ax.set_xticklabels( xticklabels )
+ax.set_yticklabels( yticklabels )
+
+ax.set_xlabel( r'$R_{s}\ \left[\mathrm{km}\right]$' )
+ax.set_ylabel( r'$M\ \left[\mathrm{M}_{\odot}\right]$' )
+
+cbar = fig.colorbar( im )
+cbar.set_label( r'$( T_{\mathrm{GR}} - T_{\mathrm{NR}} ) / T_{\mathrm{GR}}$' )
+#plt.show()
+plt.savefig( 'fig.FrequencyComparison_HeatMap.png', dpi = 300 )
 
 import os
 os.system( 'rm -rf __pycache__ ' )
