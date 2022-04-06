e482b06ce76706cb39d43970d03f917eefe380bb
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-04-06 19:00:27 -0400

    backup /home/kkadoogan/Work/Analysis/AccretionShockStudy on Wed Apr  6 07:00:27 PM EDT 2022

--- FrequencyComparison.py
+++ FrequencyComparison.py
@@ -12,6 +12,7 @@ Root = '/home/kkadoogan/Work/Codes/thornado/SandBox/AMReX/Euler_Relativistic_IDE
 M    = np.linspace( 1.0, 3.0, 11 )
 Mdot = np.array( [ 0.3 ], np.float64 )
 Rs   = np.linspace( 110, 200, 10 )
+T_GR = np.loadtxt( 'T_GR.dat' )
 
 TS = TimeScales()
 
@@ -36,18 +37,20 @@ T_Mul = np.loadtxt( 'T_Mul.dat' )
 size = 10
 fig, ax = plt.subplots( 1, 1 )
 extent = [ Rs.min()-5, Rs.max()+5, M.min()-0.1, M.max()+0.1 ]
+vmin = 10
+vmax = 80
 im = ax.imshow( T_Mul, \
                 origin = 'lower', \
                 extent = extent, \
                 cmap = 'viridis', \
-                vmin = 10, vmax = 80, \
-                interpolation = 'none', \
+                vmin = vmin, vmax = vmax, \
+                interpolation = 'bilinear', \
                 aspect = 'auto' )
 
 # from: https://matplotlib.org/3.5.0/gallery/images_contours_and_fields/
 #       contour_label_demo.html
 def fmt(x):
-    s = f"{x:g}"
+    s = f"{x:g} ms"
     return s
 
 contour = ax.contour( T_Mul, extent = extent, colors = 'white' )
@@ -70,6 +73,15 @@ ax.set_ylabel( r'$M\ \left[\mathrm{M}_{\odot}\right]$', fontsize = size )
 cbar = fig.colorbar( im, pad = 0.01, location='top' )
 cbar.set_label( r'$T_{\mathrm{SASI}}$ [ms]', size = size+2, labelpad = 10 )
 cbar.ax.tick_params( labelsize = size )
-plt.show()
+
+M = np.array( [ 1.4, 2.0, 2.8 ], np.float64 )
+Rs = np.array( [ 120.0, 150.0, 180.0 ], np.float64 )
+for m in range( M.shape[0] ):
+    for rs in range( Rs.shape[0] ):
+        ax.scatter( Rs[rs], M[m], c=T_GR[m,rs], vmin = vmin, vmax = vmax, \
+                    edgecolors = 'k' )
+
+#plt.show()
+plt.savefig( 'fig.T_SASI.png', dpi = 300 )
 import os
 os.system( 'rm -rf __pycache__ ' )
