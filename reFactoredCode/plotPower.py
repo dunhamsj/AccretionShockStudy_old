62f29fc4dc41c2c4caaa7c1d13a17d3ec8bc8e01
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-04-20 19:00:24 -0400

    backup /home/kkadoogan/Work/Analysis/AccretionShockStudy on Thu Apr 20 07:00:24 PM EDT 2023

--- reFactoredCode/plotPower.py
+++ reFactoredCode/plotPower.py
@@ -9,8 +9,12 @@ from FitPowerToModel import FittingFunction
 from computeTimeScales import ComputeTimeScales
 
 stage   = 'late'
-vsTau   = False
+vsTau   = True
 saveFig = True
+
+arrShape = (2,3)
+
+indd = np.empty( arrShape, np.int64 )
 
 if stage == 'early':
 
@@ -18,6 +22,13 @@ if stage == 'early':
     M    = np.array( [ '1.4' ], str )
     Rs   = np.array( [ '1.20e2', '1.50e2', '1.75e2' ], str )
     Rpns = np.array( [ '040' ], str )
+    # RsMax < 1.45 * Rs(t=0)
+    indd[0,0] = 847
+    indd[0,1] = 1473
+    indd[0,2] = 2033
+    indd[1,0] = 1071
+    indd[1,1] = -1
+    indd[1,2] = 2294
     suffix = ''
 
 elif stage == 'late':
@@ -26,22 +37,20 @@ elif stage == 'late':
     M    = np.array( [ '2.8' ], str )
     Rs   = np.array( [ '6.00e1', '7.00e1', '8.75e1' ], str )
     Rpns = np.array( [ '020' ], str )
+    # RsMax < 1.45 * Rs(t=0)
+    indd[0,0] = 217
+    indd[0,1] = 317
+    indd[0,2] = 510
+    indd[1,0] = -1
+    indd[1,1] = -1
+    indd[1,2] = -1
     suffix = ''
 
 else:
 
     exit('Ya done fucked up')
 
-arrShape = (R.shape[0],M.shape[0],Rs.shape[0])
-
 ID = np.empty( arrShape, object )
-
-indd = np.empty( arrShape, np.int64 )
-indd[:,:,:] = -1
-#indd[0,0,0] = 777
-#indd[0,0,1] = 1330
-#indd[0,0,2] = 1839
-#indd[1] = np.copy( indd[0] )
 
 t  = np.empty( arrShape, object )
 P0 = np.empty( arrShape, object )
@@ -65,20 +74,20 @@ for r in range( R.shape[0] ):
     for m in range( M.shape[0] ):
         for rs in range( Rs.shape[0] ):
 
-            ID[r,m,rs] \
+            ID[r,rs] \
               = '{:}2D_M{:}_Rpns{:}_Rs{:}{:}'.format \
                  ( R[r], M[m], Rpns[0], Rs[rs], suffix )
 
             plotFileDirectory \
               = '/lump/data/accretionShockStudy/newData/2D/{:}/'.format \
-                ( ID[r,m,rs] )
+                ( ID[r,rs] )
 
             if not isdir( plotFileDirectory ):
                 print( '{:} does not exist. Skipping.' \
                        .format( plotFileDirectory ) )
                 continue
 
-            plotFileBaseName = '{:}.plt'.format( ID[r,m,rs] )
+            plotFileBaseName = '{:}.plt'.format( ID[r,rs] )
             rInner = np.float64( Rpns[0] )
             rOuter = np.float64( Rs[rs] )
             tAd = 0
@@ -88,22 +97,22 @@ for r in range( R.shape[0] ):
                   ( plotFileDirectory+plotFileBaseName+'00000000', \
                     rInner, rOuter, R[r] )
 
-            T_SASI[r,m,rs] = tAd + tAc
+            T_SASI[r,rs] = tAd + tAc
 
             dataFileName \
-              = '.{:}_LegendrePowerSpectrum.dat'.format( ID[r,m,rs] )
+              = '.{:}_LegendrePowerSpectrum.dat'.format( ID[r,rs] )
 
             if not isfile( dataFileName ):
                 print( '{:} does not exist. Skipping.' \
                        .format( dataFileName ) )
                 continue
 
-            t [r,m,rs], \
-            P0[r,m,rs], \
-            P1[r,m,rs], \
-            P2[r,m,rs], \
-            P3[r,m,rs], \
-            P4[r,m,rs] \
+            t [r,rs], \
+            P0[r,rs], \
+            P1[r,rs], \
+            P2[r,rs], \
+            P3[r,rs], \
+            P4[r,rs] \
               = np.loadtxt( dataFileName )
 
             # Read in fit data
@@ -116,12 +125,12 @@ for r in range( R.shape[0] ):
             tmp \
               = np.array( list( map( np.float64, s[ind:].split() ) ), \
                           np.float64 )
-            t0    [r,m,rs] = tmp[0]
-            t1    [r,m,rs] = tmp[1]
-            LogF  [r,m,rs] = tmp[2]
-            omegaR[r,m,rs] = tmp[3]
-            omegaI[r,m,rs] = tmp[4]
-            delta [r,m,rs] = tmp[5]
+            t0    [r,rs] = tmp[0]
+            t1    [r,rs] = tmp[1]
+            LogF  [r,rs] = tmp[2]
+            omegaR[r,rs] = tmp[3]
+            omegaI[r,rs] = tmp[4]
+            delta [r,rs] = tmp[5]
 
             dum = f.readline()
 
@@ -129,8 +138,8 @@ for r in range( R.shape[0] ):
             tmp \
               = np.array( list( map( np.float64, s[ind:].split() ) ), \
                           np.float64 )
-            omegaR_err[r,m,rs] = tmp[1]
-            omegaI_err[r,m,rs] = tmp[2]
+            omegaR_err[r,rs] = tmp[1]
+            omegaI_err[r,rs] = tmp[2]
 
             f.close()
 
@@ -146,35 +155,35 @@ for r in range( R.shape[0] ):
     for rs in range( Rs.shape[0] ):
 
         dataFileName \
-          = '.{:}_LegendrePowerSpectrum.dat'.format( ID[r,m,rs] )
+          = '.{:}_LegendrePowerSpectrum.dat'.format( ID[r,rs] )
 
         if not isfile( dataFileName ):
             print( '{:} does not exist. Skipping.' \
                    .format( dataFileName ) )
             continue
 
-        tt  = t [r,m,rs]
-        P1t = P1[r,m,rs]
-        t0t = t0[r,m,rs]
-        t1t = t1[r,m,rs]
+        tt  = t [r,rs]
+        P1t = P1[r,rs]
+        t0t = t0[r,rs]
+        t1t = t1[r,rs]
 
-        tt  = np.copy( tt [0:indd[r,m,rs]] )
-        P1t = np.copy( P1t[0:indd[r,m,rs]] )
+        tt  = np.copy( tt [0:indd[r,rs]] )
+        P1t = np.copy( P1t[0:indd[r,rs]] )
 
         indF = np.where( ( tt >= t0t ) & ( tt <= t1t ) )[0]
 
         tF = tt[indF]
 
-        logFt   = LogF  [r,m,rs]
-        omegaRt = omegaR[r,m,rs]
-        omegaIt = omegaI[r,m,rs]
-        deltat  = delta [r,m,rs]
+        logFt   = LogF  [r,rs]
+        omegaRt = omegaR[r,rs]
+        omegaIt = omegaI[r,rs]
+        deltat  = delta [r,rs]
 
 #        F = np.exp( FittingFunction \
 #                      ( tF - tF[0], logFt, \
 #                        omegaRt, omegaIt, deltat ) )
 
-        tau = T_SASI[r,m,rs]
+        tau = T_SASI[r,rs]
 
         ind = np.where( tt/tau <= 10.0 )[0]
 
@@ -182,10 +191,10 @@ for r in range( R.shape[0] ):
 
         if R.shape[0] == 1:
             axs.plot( tt[ind]/tau, P1t[ind], '-', color = color[rs], \
-                      label = r'$\texttt{{{:}}}$'.format( ID[r,m,rs] ) )
+                      label = r'$\texttt{{{:}}}$'.format( ID[r,rs] ) )
         else:
             axs[r].plot( tt[ind]/tau, P1t[ind], '-', color = color[rs], \
-                         label = r'$\texttt{{{:}}}$'.format( ID[r,m,rs] ) )
+                         label = r'$\texttt{{{:}}}$'.format( ID[r,rs] ) )
             #axs[r].plot( tF/tau, F  , '-', color = 'k' )
 
         # END for rs in range( Rs.shape[0] )
--- reFactoredCode/shockRadius.py
+++ reFactoredCode/shockRadius.py
@@ -128,11 +128,16 @@ if __name__ == "__main__":
     #rootDirectory = '/lump/data/accretionShockStudy/'
     rootDirectory = '/lump/data/accretionShockStudy/newData/2D/'
 
+#    M    = [ '1.4' ]
+#    Rs   = [ '1.20e2', '1.50e2', '1.75e2' ]
+#    xL   = [ '040' ]
+
+    M    = [ '2.8' ]
+    Rs   = [ '6.00e1', '7.00e1', '8.75e1' ]
+    xL   = [ '020' ]
+
     rel  = [ 'NR', 'GR', ]
-    M    = [ '1.4' ]
     Mdot = [ '0.3' ]
-    Rs   = [ '1.75e2' ]
-    xL   = [ '040' ]
     suffix = [ '' ]
     xH   = [ 1.5 * x for x in np.float64( Rs ) ]
 
@@ -144,47 +149,48 @@ if __name__ == "__main__":
              '#999999', '#e41a1c', '#dede00']
 
     for i in range( len( rel ) ):
+        for j in range( len( Rs ) ):
 
-        ID = '{:}2D_M{:}_Rpns{:}_Rs{:}{:}' \
-             .format( rel[i], M[0], xL[0], Rs[0], suffix[0] )
+            ID = '{:}2D_M{:}_Rpns{:}_Rs{:}{:}' \
+                 .format( rel[i], M[0], xL[0], Rs[j], suffix[0] )
 
-        plotfileDirectory = rootDirectory + ID + '/'
-        plotfileBaseName = ID + '.plt'
-        entropyThreshold = 1.0e15
+            plotfileDirectory = rootDirectory + ID + '/'
+            plotfileBaseName = ID + '.plt'
+            entropyThreshold = 1.0e15
 
-        #MakeLineOutPlot \
-        #  ( plotfileDirectory, plotfileBaseName, entropyThreshold )
+            #MakeLineOutPlot \
+            #  ( plotfileDirectory, plotfileBaseName, entropyThreshold )
 
-        dataFileName = '.{:}_ShockRadiusVsTime.dat'.format( ID )
-        forceChoice = True
-        OW = True
-        MakeDataFile \
-          ( plotfileDirectory, plotfileBaseName, dataFileName, \
-            entropyThreshold, markEvery = 1, forceChoice = forceChoice, \
-            OW = OW )
+            dataFileName = '.{:}_ShockRadiusVsTime.dat'.format( ID )
+            forceChoice = True
+            OW = False
+            MakeDataFile \
+              ( plotfileDirectory, plotfileBaseName, dataFileName, \
+                entropyThreshold, markEvery = 1, forceChoice = forceChoice, \
+                OW = OW )
 
-        Time, RsAve, RsMin, RsMax = np.loadtxt( dataFileName )
-        Time  = np.copy( Time [:-1] )
-        RsAve = np.copy( RsAve[:-1] )
-        RsMin = np.copy( RsMin[:-1] )
-        RsMax = np.copy( RsMax[:-1] )
+            Time, RsAve, RsMin, RsMax = np.loadtxt( dataFileName )
+            Time  = np.copy( Time [:-1] )
+            RsAve = np.copy( RsAve[:-1] )
+            RsMin = np.copy( RsMin[:-1] )
+            RsMax = np.copy( RsMax[:-1] )
 
-        ind = -1
-        ind = np.where( RsMax > 1.45 * np.float64( Rs[0] ) )[0]
-        if not len( ind ) == 0:
-            ind = np.copy( ind[0] )
-        else:
             ind = -1
-        print( ID, ind, Time[ind] )
+            ind = np.where( RsMax > 1.45 * np.float64( Rs[j] ) )[0]
+            if not len( ind ) == 0:
+                ind = np.copy( ind[0] )
+            else:
+                ind = -1
+            print( ID, ind, Time[ind] )
 
-        c = i
-        ax.plot( Time[0:ind], RsAve[0:ind] / RsAve[0], \
-                c = color[c], ls = '-' , \
-                label = '{:}'.format( rel[i] ) )
-        ax.plot( Time[0:ind], RsMin[0:ind] / RsAve[0], \
-                 c = color[c], ls = '--', label = 'min' )
-        ax.plot( Time[0:ind], RsMax[0:ind] / RsAve[0], \
-                 c = color[c], ls = ':' , label = 'max' )
+            c = i
+            ax.plot( Time[0:ind], RsAve[0:ind] / RsAve[0], \
+                    c = color[c], ls = '-' , \
+                    label = '{:}'.format( rel[i] ) )
+            ax.plot( Time[0:ind], RsMin[0:ind] / RsAve[0], \
+                     c = color[c], ls = '--', label = 'min' )
+            ax.plot( Time[0:ind], RsMax[0:ind] / RsAve[0], \
+                     c = color[c], ls = ':' , label = 'max' )
 
     ax.set_xlabel( 'Time [ms]' )
     ax.set_ylabel( r'$R_{\mathrm{S}}/R_{\mathrm{S}}\left(0\right)$', \
